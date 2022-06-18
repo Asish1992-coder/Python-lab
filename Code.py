79d85a64f907d4c6e28b6eb9a4b8da2c3b5e7320
Asish <Asish.kar1992@gmail.com>
2022-06-18 17:31:49 +0530

    This is to find out Odd / Even number

---
+++ Code.py
@@ -0,0 +1,10 @@
+Ask1 = input("Welcome to Python Class! May I have your name: ")
+print("Welcome to the class "+ Ask1+"!")
+Num1 = int(input("Choose your preferable number: "))
+print (f"your number is {Num1}!")
+Num2 = Num1 % 2
+
+if Num2 == 1:
+    print("It's a odd one")
+else:
+    print("It is a Even number")
